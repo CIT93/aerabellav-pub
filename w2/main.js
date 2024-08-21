5c504624c4836284b508f683437f72adc9c1c7ca
AVargas0502 <amvargas11@my.scccd.edu>
2024-08-21 22:26:54 +0000

    variables

---
+++ w2/main.js
@@ -0,0 +1,18 @@
+console.log('Hello World!')
+
+const myHeading = document.querySelector("h2");
+myHeading.textContent = "Hello world!";
+
+//single line comment
+/* block multi line comment */
+
+// coding challenge - will this code work?
+// Yes, it will work
+
+// naming standard is called camelCase is just in this case
+
+const myNumber = 42;
+let myString = 'Hello';
+// myString = 43; not a good idea
+//general code for variable declaration
+// your understanding
