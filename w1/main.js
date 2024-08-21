6023a009d025cd54b2fe5931be76b6a18bc15378
AVargas0502 <amvargas11@my.scccd.edu>
2024-08-21 22:05:23 +0000

    comments and code challenge

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,10 @@
 console.log('Hello World!')
+
+const myHeading = document.querySelector("h2");
+myHeading.textContent = "Hello world!";
+
+//single line comment
+/* block multi line comment */
+
+// coding challenge - will this code work?
+// Yes, it will work
